a0f2d1cdd3126fe6a8c3506917a3aff5f5611ae3
Kamilla Saleniece <eldzhazo@gmai.com>
2023-04-16 22:36:43 +0300

    ==

--- hash_substring.py
+++ hash_substring.py
@@ -32,8 +32,7 @@ def rabikraps(text, pattern):
 text = None
 pattern = None
 input_choice = input()
-print(input_choice == "I")
-print(input_choice.equals("I"))
+print(input_choice.lstrip().rstrip() == 'I')
 if input_choice == "I":
     pattern = input().rstrip()
     #\r\n\|??
