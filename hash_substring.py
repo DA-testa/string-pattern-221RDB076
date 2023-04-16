76b28ac628561d423b0691b04e1fcc9c5a05474c
221RDB076 <125807862+221RDB076@users.noreply.github.com>
2023-04-16 22:13:08 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,53 +1,43 @@
-def rabikrap(text, pattern):
+def rabikraps(text, pattern):
 
-    galv = 101  
-    x = 256  
-    p = len(pattern)
-    t = len(text)
-    c = 0 
-    d = 0 
-    e = 1
+    prime = 101  
+    d = 256  
+    pp = len(pattern)
+    tt = len(text)
+    p = 0 
+    t = 0 
+    h = 1
+    for i in range(pp - 1):
+        h = (h * d) % prime   
+    for i in range(pp):
+        p = (d * p + ord(pattern[i])) % prime
+        t = (d * t + ord(text[i])) % prime
 
-    for i in range(p - 1):
-        e = (e * x) % galv
-
-    for i in range(p):
-        c = (x * c + ord(pattern[i])) % galv
-        d = (x * d + ord(text[i])) % galv
-
-    notikumi = [] 
-
-
-    for i in range(t - p + 1):
-
-        if c == d:
+    notikumi = []
+    for i in range(tt - pp + 1): 
+        if p == t:
             match = True
-            for j in range(p):
+            for j in range(pp):
                 if text[i + j] != pattern[j]:
                     match = False
                     break
             if match:
                 notikumi.append(i)
-
-
-        if i < t - p:
-            d = (x * (d - ord(text[i]) * e) + ord(text[i + p])) % galv
-
-        
-            if d < 0:
-                d = d + galv
-
+        if i < tt - pp:
+            t = (d * (t - ord(text[i]) * h) + ord(text[i + pp])) % prime
+      
+            if t < 0:
+                t = t + prime
     return notikumi
 
-ievgalv = input()
-if ievgalv == "I":
+input_choice = input()
+if input_choice == "I":
     pattern = input().rstrip()
     text = input().rstrip()
-elif ievgalv == "F":
-    with open("test_sample.txt") as file:
+elif input_choice == "F":
+    with open("texttest.txt") as file:
         pattern = file.readline().rstrip()
         text = file.readline().rstrip()
 
-
-notikumi = rabikrap(text, pattern)
+notikumi = rabikraps(text, pattern)
 print(*notikumi)
